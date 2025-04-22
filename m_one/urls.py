6deb4dfda877bcac5aa2c174dcd0f0a2f2e9dba3
Wilko <wilkorietveld@icloud.com>
2025-04-22 14:37:24 +0200

    🧹 Clean: remove local project folders from repo

--- m_one/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path("dashboard/", include("dashboard.urls")),
-    path("admin/", admin.site.urls),
-    path("pupdater/", include("pupdater.urls")),  # ✅ dit is cruciaal
-]
